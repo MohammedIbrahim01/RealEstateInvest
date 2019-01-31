ec68b243177f147247c68d5b8a260b3190dec723
Ahmed Khairy <Ahmedkhairyitpro@gmail.com>
2019-01-31 16:06:36 +0200

    Apartment details complete

--- app/src/main/java/com/abdelazim/x/realestateinvest/apartment_details/ApartmentDetailsFragment.java
+++ app/src/main/java/com/abdelazim/x/realestateinvest/apartment_details/ApartmentDetailsFragment.java
@@ -1,13 +1,16 @@
 package com.abdelazim.x.realestateinvest.apartment_details;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.abdelazim.x.realestateinvest.R;
+import com.abdelazim.x.realestateinvest.contact_us.ContactUsFragment;
 
 
 /**
@@ -15,6 +18,7 @@ import com.abdelazim.x.realestateinvest.R;
  */
 public class ApartmentDetailsFragment extends Fragment {
 
+    Button favourite,call;
 
     public ApartmentDetailsFragment() {
         // Required empty public constructor
@@ -25,7 +29,32 @@ public class ApartmentDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        calling();
+        handling();
         return inflater.inflate(R.layout.fragment_apartment_details, container, false);
+
+
+
+    }
+
+    public void calling(){
+        favourite=getActivity().findViewById(R.id.favourite);
+        call=getActivity().findViewById(R.id.call);
+
+
+    }
+
+    public void handling(){
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), ContactUsFragment.class));
+            }
+        });
+
+
     }
 
 }
